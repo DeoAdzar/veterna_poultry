3b4eb9d23a7408eab6219c6c5c1d3b246dbbbf42
DeoAdzarN <deo.nugraha@woilo.com>
2023-06-20 20:35:58 +0700

    update appbar cart page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../widgets/shop/cart_products.dart';
 import '../widgets/shop/cart_total.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -8,6 +10,24 @@ class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: const Icon(Icons.arrow_back_ios_new),
+          color: Colors.black,
+        ),
+        title: Text(
+          "Keranjang",
+          textAlign: TextAlign.center,
+          style: GoogleFonts.inter(
+              color: Colors.black,
+              fontSize: 20.0,
+              fontWeight: FontWeight.normal),
+        ),
+        elevation: 0.0,
+      ),
       body: SafeArea(
         child: Column(
           children: [
